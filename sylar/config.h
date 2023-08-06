73178c59d9fed976490beb05d30a95c1d3dd075c
Guoli <972486208@qq.com>
2023-08-07 00:05:53 +0800

    finish the STL support. but remian the fault about the same name different value

--- sylar/config.h
+++ sylar/config.h
@@ -15,162 +15,302 @@
 
 namespace sylar {
 class ConfigVarBase {
- public:
-  typedef std::shared_ptr<ConfigVarBase> ptr;
-  ConfigVarBase(const std::string& name, const std::string description = "")
-      : m_name(name), m_description(description) {
-    std::transform(m_name.begin(), m_name.end(), m_name.begin(), tolower);
-  }
-  virtual ~ConfigVarBase() {}
-  const std::string& getName() const { return m_name; }
-  const std::string& getDescription() const { return m_description; }
-  virtual std::string toString() = 0;
-  virtual bool fromString(const std::string& val) = 0;
+public:
+    typedef std::shared_ptr<ConfigVarBase> ptr;
+    ConfigVarBase(const std::string& name, const std::string description = "")
+        : m_name(name)
+        , m_description(description)
+    {
+        std::transform(m_name.begin(), m_name.end(), m_name.begin(), tolower);
+    }
+    virtual ~ConfigVarBase() { }
+    const std::string& getName() const { return m_name; }
+    const std::string& getDescription() const { return m_description; }
+    virtual std::string toString() = 0;
+    virtual bool fromString(const std::string& val) = 0;
 
- protected:
-  std::string m_name;
-  std::string m_description;
+protected:
+    std::string m_name;
+    std::string m_description;
 };
 // F from_type, T to_type
 template <class F, class T>
 class LexicalCast {
- public:
-  T operator()(const F& v) { return boost::lexical_cast<T>(v); }
+public:
+    T operator()(const F& v) { return boost::lexical_cast<T>(v); }
+};
+
+template <class T>
+class LexicalCast<std::string, std::unordered_map<std::string, T>> {
+public:
+    std::unordered_map<std::string, T> operator()(const std::string& v)
+    {
+        YAML::Node node = YAML::Load(v);
+        typename std::unordered_map<std::string, T> vec;
+        std::stringstream ss;
+        for (auto it = node.begin(); it != node.end(); it++) {
+            ss.str("");
+            ss << it->second;
+            vec.insert(std::make_pair(it->first.Scalar(), LexicalCast<std::string, T>()(ss.str())));
+        }
+        return vec;
+    }
+};
+template <class T>
+class LexicalCast<std::unordered_map<std::string, T>, std::string> {
+public:
+    std::string operator()(const std::unordered_map<std::string, T>& v)
+    {
+        YAML::Node node;
+        for (auto& i : v) {
+            node[i.first] = YAML::Load(LexicalCast<T, std::string>()(i.second));
+        }
+        std::stringstream ss;
+        ss << node;
+
+        return ss.str();
+    }
+};
+
+template <class T>
+class LexicalCast<std::string, std::map<std::string, T>> {
+public:
+    std::map<std::string, T> operator()(const std::string& v)
+    {
+        YAML::Node node = YAML::Load(v);
+        typename std::map<std::string, T> vec;
+        std::stringstream ss;
+        for (auto it = node.begin(); it != node.end(); it++) {
+            ss.str("");
+            ss << it->second;
+            vec.insert(std::make_pair(it->first.Scalar(), LexicalCast<std::string, T>()(ss.str())));
+        }
+        return vec;
+    }
+};
+template <class T>
+class LexicalCast<std::map<std::string, T>, std::string> {
+public:
+    std::string operator()(const std::map<std::string, T>& v)
+    {
+        YAML::Node node;
+        for (auto& i : v) {
+            node[i.first] = YAML::Load(LexicalCast<T, std::string>()(i.second));
+        }
+        std::stringstream ss;
+        ss << node;
+
+        return ss.str();
+    }
+};
+
+template <class T>
+class LexicalCast<std::string, std::unordered_set<T>> {
+public:
+    std::unordered_set<T> operator()(const std::string& v)
+    {
+        YAML::Node node = YAML::Load(v);
+        typename std::unordered_set<T> vec;
+        std::stringstream ss;
+        for (size_t i = 0; i < node.size(); ++i) {
+            ss.str("");
+            ss << node[i];
+            vec.insert(LexicalCast<std::string, T>()(ss.str()));
+        }
+        return vec;
+    }
+};
+template <class T>
+class LexicalCast<std::unordered_set<T>, std::string> {
+public:
+    std::string operator()(const std::unordered_set<T>& v)
+    {
+        YAML::Node node;
+        for (auto& i : v) {
+            node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+        }
+        std::stringstream ss;
+        ss << node;
+
+        return ss.str();
+    }
+};
+
+template <class T>
+class LexicalCast<std::string, std::set<T>> {
+public:
+    std::set<T> operator()(const std::string& v)
+    {
+        YAML::Node node = YAML::Load(v);
+        typename std::set<T> vec;
+        std::stringstream ss;
+        for (size_t i = 0; i < node.size(); ++i) {
+            ss.str("");
+            ss << node[i];
+            vec.insert(LexicalCast<std::string, T>()(ss.str()));
+        }
+        return vec;
+    }
+};
+template <class T>
+class LexicalCast<std::set<T>, std::string> {
+public:
+    std::string operator()(const std::set<T>& v)
+    {
+        YAML::Node node;
+        for (auto& i : v) {
+            node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+        }
+        std::stringstream ss;
+        ss << node;
+
+        return ss.str();
+    }
 };
 
 template <class T>
 class LexicalCast<std::string, std::list<T>> {
- public:
-  std::list<T> operator()(const std::string& v) {
-    YAML::Node node = YAML::Load(v);
-    typename std::list<T> vec;
-    std::stringstream ss;
-    for (size_t i = 0; i < node.size(); ++i) {
-      ss.str("");
-      ss << node[i];
-      vec.push_back(LexicalCast<std::string, T>()(ss.str()));
+public:
+    std::list<T> operator()(const std::string& v)
+    {
+        YAML::Node node = YAML::Load(v);
+        typename std::list<T> vec;
+        std::stringstream ss;
+        for (size_t i = 0; i < node.size(); ++i) {
+            ss.str("");
+            ss << node[i];
+            vec.push_back(LexicalCast<std::string, T>()(ss.str()));
+        }
+        return vec;
     }
-    return vec;
-  }
 };
 template <class T>
 class LexicalCast<std::list<T>, std::string> {
- public:
-  std::string operator()(const std::list<T>& v) {
-    YAML::Node node;
-    for (auto& i : v) {
-      node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
-    }
-    std::stringstream ss;
-    ss << node;
+public:
+    std::string operator()(const std::list<T>& v)
+    {
+        YAML::Node node;
+        for (auto& i : v) {
+            node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+        }
+        std::stringstream ss;
+        ss << node;
 
-    return ss.str();
-  }
+        return ss.str();
+    }
 };
 
 template <class T>
 class LexicalCast<std::string, std::vector<T>> {
- public:
-  std::vector<T> operator()(const std::string& v) {
-    YAML::Node node = YAML::Load(v);
-    typename std::vector<T> vec;
-    std::stringstream ss;
-    for (size_t i = 0; i < node.size(); ++i) {
-      ss.str("");
-      ss << node[i];
-      vec.push_back(LexicalCast<std::string, T>()(ss.str()));
+public:
+    std::vector<T> operator()(const std::string& v)
+    {
+        YAML::Node node = YAML::Load(v);
+        typename std::vector<T> vec;
+        std::stringstream ss;
+        for (size_t i = 0; i < node.size(); ++i) {
+            ss.str("");
+            ss << node[i];
+            vec.push_back(LexicalCast<std::string, T>()(ss.str()));
+        }
+        return vec;
     }
-    return vec;
-  }
 };
 template <class T>
 class LexicalCast<std::vector<T>, std::string> {
- public:
-  std::string operator()(const std::vector<T>& v) {
-    YAML::Node node;
-    for (auto& i : v) {
-      node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
-    }
-    std::stringstream ss;
-    ss << node;
+public:
+    std::string operator()(const std::vector<T>& v)
+    {
+        YAML::Node node;
+        for (auto& i : v) {
+            node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+        }
+        std::stringstream ss;
+        ss << node;
 
-    return ss.str();
-  }
+        return ss.str();
+    }
 };
 
 // FromStr
 template <class T, class FromStr = LexicalCast<std::string, T>,
-          class ToStr = LexicalCast<T, std::string>>
+    class ToStr = LexicalCast<T, std::string>>
 class ConfigVar : public ConfigVarBase {
- public:
-  typedef std::shared_ptr<ConfigVar> ptr;
-  ConfigVar(const std::string& name, const T& default_value,
-            const std::string& description = "")
-      : ConfigVarBase(name, description), m_val(default_value) {}
-  std::string toString() override {
-    try {
-      // return boost::lexical_cast<std::string>(m_val);
-      return ToStr()(m_val);
-    } catch (std::exception& e) {
-      SYLAR_LOG_ERROR(SYLAR_LOG_ROOT())
-          << "ConfigVar::toString exception" << e.what()
-          << " convert: " << typeid(m_val).name() << " to string";
+public:
+    typedef std::shared_ptr<ConfigVar> ptr;
+    ConfigVar(const std::string& name, const T& default_value,
+        const std::string& description = "")
+        : ConfigVarBase(name, description)
+        , m_val(default_value)
+    {
     }
-    return "";
-  }
-  bool fromString(const std::string& val) override {
-    try {
-      // m_val = boost::lexical_cast<T>(val);
-      setValue(FromStr()(val));
-    } catch (std::exception& e) {
-      SYLAR_LOG_ERROR(SYLAR_LOG_ROOT())
-          << "ConfigVar::fromString exception" << e.what()
-          << " convert: string to " << typeid(m_val).name();
+    std::string toString() override
+    {
+        try {
+            // return boost::lexical_cast<std::string>(m_val);
+            return ToStr()(m_val);
+        } catch (std::exception& e) {
+            SYLAR_LOG_ERROR(SYLAR_LOG_ROOT())
+                << "ConfigVar::toString exception" << e.what()
+                << " convert: " << typeid(m_val).name() << " to string";
+        }
+        return "";
     }
-    return false;
-  }
-  const T getValue() const { return m_val; }
-  void setValue(const T& val) { m_val = val; }
+    bool fromString(const std::string& val) override
+    {
+        try {
+            // m_val = boost::lexical_cast<T>(val);
+            setValue(FromStr()(val));
+        } catch (std::exception& e) {
+            SYLAR_LOG_ERROR(SYLAR_LOG_ROOT())
+                << "ConfigVar::fromString exception" << e.what()
+                << " convert: string to " << typeid(m_val).name();
+        }
+        return false;
+    }
+    const T getValue() const { return m_val; }
+    void setValue(const T& val) { m_val = val; }
 
- private:
-  T m_val;
+private:
+    T m_val;
 };
 class Config {
- public:
-  typedef std::map<std::string, ConfigVarBase::ptr> ConfigVarMap;
-  // give value while defining
-  template <class T>
-  static typename ConfigVar<T>::ptr Lookup(
-      const std::string& name, const T& default_value,
-      const std::string& description = "") {
-    auto tmp = Lookup<T>(name);
-    if (tmp) {
-      SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "Lookup name=" << name << " exists";
-      return tmp;
+public:
+    typedef std::map<std::string, ConfigVarBase::ptr> ConfigVarMap;
+    // give value while defining
+    template <class T>
+    static typename ConfigVar<T>::ptr Lookup(
+        const std::string& name, const T& default_value,
+        const std::string& description = "")
+    {
+        auto tmp = Lookup<T>(name);
+        if (tmp) {
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "Lookup name=" << name << " exists";
+            return tmp;
+        }
+        if (name.find_first_not_of("abcdefghijklmnopqrstuvwxyz._0123456789") != std::string::npos) {
+            SYLAR_LOG_ERROR(SYLAR_LOG_ROOT()) << "Lookup name invalid " << name;
+            throw std::invalid_argument(name);
+        }
+        typename ConfigVar<T>::ptr v(
+            new ConfigVar<T>(name, default_value, description));
+        s_datas[name] = v;
+        return v;
     }
-    if (name.find_first_not_of("abcdefghijklmnopqrstuvwxyz._0123456789") !=
-        std::string::npos) {
-      SYLAR_LOG_ERROR(SYLAR_LOG_ROOT()) << "Lookup name invalid " << name;
-      throw std::invalid_argument(name);
+    // find
+    template <class T>
+    static typename ConfigVar<T>::ptr Lookup(const std::string& name)
+    {
+        auto it = s_datas.find(name);
+        if (it == s_datas.end()) {
+            return nullptr;
+        }
+        return std::dynamic_pointer_cast<ConfigVar<T>>(it->second);
     }
-    typename ConfigVar<T>::ptr v(
-        new ConfigVar<T>(name, default_value, description));
-    s_datas[name] = v;
-    return v;
-  }
-  // find
-  template <class T>
-  static typename ConfigVar<T>::ptr Lookup(const std::string& name) {
-    auto it = s_datas.find(name);
-    if (it == s_datas.end()) {
-      return nullptr;
-    }
-    return std::dynamic_pointer_cast<ConfigVar<T>>(it->second);
-  }
-  static void LoadFromYaml(const YAML::Node& root);
-  static ConfigVarBase::ptr LookupBase(const std::string& name);
+    static void LoadFromYaml(const YAML::Node& root);
+    static ConfigVarBase::ptr LookupBase(const std::string& name);
 
- private:
-  static ConfigVarMap s_datas;
+private:
+    static ConfigVarMap s_datas;
 };
-}  // namespace sylar
+} // namespace sylar
--- tests/test_config.cc
+++ tests/test_config.cc
@@ -2,15 +2,30 @@
 
 #include "../sylar/config.h"
 #include "../sylar/log.h"
-sylar::ConfigVar<int>::ptr g_int_value_config = sylar::Config::Lookup("system.port", (int)8080, "system port");
-sylar::ConfigVar<float>::ptr g_float_value_config = sylar::Config::Lookup("system.value", (float)10.2f, "system value");
+sylar::ConfigVar<int>::ptr g_int_value_config
+    = sylar::Config::Lookup("system.port", (int)8080, "system port");
 
-sylar::ConfigVar<std::vector<int>>::ptr g_int_vector_config = sylar::Config::Lookup("system.int_vec",
-    std::vector<int> { 1, 2 },
-    "system int vec");
-sylar::ConfigVar<std::list<int>>::ptr g_int_list_config = sylar::Config::Lookup("system.int_list",
-    std::list<int> { 1, 2 },
-    "system int list");
+sylar::ConfigVar<float>::ptr g_float_value_config
+    = sylar::Config::Lookup("system.value", (float)10.2f, "system value");
+
+sylar::ConfigVar<std::vector<int>>::ptr g_int_vector_config
+    = sylar::Config::Lookup("system.int_vec", std::vector<int> { 1, 2 }, "system int vec");
+
+sylar::ConfigVar<std::list<int>>::ptr g_int_list_config
+    = sylar::Config::Lookup("system.int_list", std::list<int> { 1, 2 }, "system int list");
+
+sylar::ConfigVar<std::set<int>>::ptr g_int_set_config
+    = sylar::Config::Lookup("system.int_set", std::set<int> { 1, 2 }, "system int set");
+
+sylar::ConfigVar<std::unordered_set<int>>::ptr g_int_unordered_set_config
+    = sylar::Config::Lookup("system.int_uset", std::unordered_set<int> { 1, 2 }, "system int unordered_set");
+
+// init list mode to init map
+sylar::ConfigVar<std::map<std::string, int>>::ptr g_int_map_value_config
+    = sylar::Config::Lookup("system.str_int_map", std::map<std::string, int> { { "k", 2 } }, "system str int map");
+
+sylar::ConfigVar<std::unordered_map<std::string, int>>::ptr g_int_umap_value_config
+    = sylar::Config::Lookup("system.str_int_umap", std::unordered_map<std::string, int> { { "k", 2 } }, "system str int umap");
 
 void print_yaml(const YAML::Node& node, int level);
 
@@ -27,23 +42,43 @@ void test_config()
         << "before: " << g_int_value_config->getValue();
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
         << "before: " << g_float_value_config->toString();
-
-    auto v = g_int_vector_config->getValue();
-    for (auto& i : v) {
-        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "before int_vec: " << i;
+#define XX(g_var, name, prefix)                                                               \
+    {                                                                                         \
+        auto& v = g_var->getValue();                                                          \
+        for (auto& i : v) {                                                                   \
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << #prefix " " #name ": " << i;                  \
+        }                                                                                     \
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << #prefix " " #name " yaml: " << g_var->toString(); \
     }
+
+#define XX_M(g_var, name, prefix)                                                                               \
+    {                                                                                                           \
+        auto& v = g_var->getValue();                                                                            \
+        for (auto& i : v) {                                                                                     \
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << #prefix " " #name ": {" << i.first << " - " << i.second << "}"; \
+        }                                                                                                       \
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << #prefix " " #name " yaml: " << g_var->toString();                   \
+    }
+
+    XX(g_int_vector_config, int_vec, before);
+    XX(g_int_list_config, int_list, before);
+    XX(g_int_set_config, int_set, before);
+    XX(g_int_unordered_set_config, int_uset, before);
+    XX_M(g_int_map_value_config, str_int_map, before);
+    XX_M(g_int_umap_value_config, str_int_umap, before);
+
     YAML::Node root = YAML::LoadFile("/home/gl/workspace/sylar/bin/conf/log.yml");
-
     sylar::Config::LoadFromYaml(root);
-    SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
-        << "after: " << g_int_value_config->getValue();
-    SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
-        << "after: " << g_float_value_config->toString();
 
-    v = g_int_vector_config->getValue();
-    for (auto& i : v) {
-        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "after int_vec: " << i;
-    }
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "after: " << g_int_value_config->getValue();
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "after: " << g_float_value_config->toString();
+
+    XX(g_int_vector_config, int_vec, after);
+    XX(g_int_list_config, int_list, after);
+    XX(g_int_set_config, int_set, after);
+    XX(g_int_unordered_set_config, int_uset, after);
+    XX_M(g_int_map_value_config, str_int_map, after);
+    XX_M(g_int_umap_value_config, str_int_umap, after);
 }
 
 void print_yaml(const YAML::Node& node, int level)
